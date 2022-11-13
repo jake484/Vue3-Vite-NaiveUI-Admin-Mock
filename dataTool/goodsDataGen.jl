380be96f57230c55e721e0b8822f4de3e30ee177
jake484 <522432938@qq.com>
2022-11-13 12:12:33 +0800

    update tools

--- dataTool/goodsDataGen.jl
+++ dataTool/goodsDataGen.jl
@@ -1,21 +1,27 @@
 using JSON3
-products = Dict(
-    "data" => Dict(
-        "code" => 200,
-        "msg" => "获取成功",
-        "success" => true,
-        "token" => "admin_token"
+
+function goodsDataGen(goodNum::Int)
+    products = Dict(
+        "data" => Dict(
+            "code" => 200,
+            "msg" => "获取成功",
+            "success" => true,
+            "token" => "admin_token"
+        )
     )
-)
-goods = Dict[]
-for i = 1:50
-    good = Dict([
-        ("userId", 0)
-        ("id", i)
-        ("title", String(UInt8.(rand(97:122, 10))))
-        ("introduce", String(UInt8.(rand(97:122, 100))))
-    ])
-    push!(goods, good)
+    goods = Dict[]
+    for i = 1:goodNum
+        good = Dict([
+            ("userId", 0)
+            ("id", i)
+            ("title", String(UInt8.(rand(97:122, 10))))
+            ("introduce", String(UInt8.(rand(97:122, 100))))
+        ])
+        push!(goods, good)
+    end
+    products["data"]["data"] = goods
+    JSON3.write(joinpath(@__DIR__, "goodsData.json"), products)
 end
-products["data"]["data"] = goods
-JSON3.write(joinpath(@__DIR__, "goodsData.json"), products)
+
+# 生成任意数量的商品，函数参数为商品个数
+goodsDataGen(200)
